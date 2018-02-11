5a20b35098ae3e27f825d0e0d7d98b1141e72c0f
KateDetsyk <detsyk.katrusia@gmail.com>
2018-02-11 20:55:18 +0200

    first commit

---
+++ prog.py
@@ -0,0 +1,93 @@
+import folium
+from geopy.geocoders import Nominatim
+
+
+year_input = int(input("Enter a year:"))
+
+geolocator = Nominatim()
+result = {}
+with open('locations.list', mode = "r") as f:
+    for line_number, line in enumerate(f.readlines()):
+        if line_number > 13:
+            # elem = line.split()
+            #
+            # year = elem[1]
+            # year = year.replace('(', '').replace(')', '')
+            # try:
+            #     year = int(year)
+            # except ValueError:
+            #     year = year.replace('(', '').replace(')', '')
+            #     year = int(year)
+            #
+            # film_name = elem[0]
+            # film_name = film_name.replace("#", "").replace("'", "").replace("\"", "")
+            #
+            # film_location = " ".join(e for e in elem[2:])
+            # if "}" in film_location:
+            #     index = film_location.index("}")
+            #     film_location = film_location[index + 1:]
+            #
+            # if "(" in film_location:
+            #     index = film_location.index("(")
+            #     film_location = film_location[:index]
+            # film_location = film_location.lstrip()
+            # film_location = film_location.strip()
+            # film_location = geolocator.geocode(film_location)
+            # film_location = [film_location.latitude, film_location.longitude]
+
+            # film name
+            index_film_name_end = line.index("(")
+            film_name = line[:index_film_name_end]
+            film_name = film_name.strip()
+
+            # film year
+            try:
+                year = line[index_film_name_end+1:index_film_name_end+5]
+                year = int(year)
+            except ValueError:
+                print("{0} does not have a year".format(film_name))
+
+            # skip useless year
+            if year != year_input:
+                continue
+
+            # locations
+            location_start = line.index(")") + 1
+            film_location = line[location_start:]
+            if "}" in film_location:
+                 index = film_location.index("}")
+                 film_location = film_location[index + 1:]
+            if "(" in film_location:
+                 index = film_location.index("(")
+                 film_location = film_location[:index]
+            film_location = film_location.strip()
+            try:
+                film_location = geolocator.geocode(film_location)
+                film_location = [film_location.latitude, film_location.longitude]
+            except AttributeError:
+                print("Sorry {0} can't be converted into coordinates.".format(film_name))
+            except:
+                print("geopy error")
+
+            # result
+            if film_location:
+                if year in result:
+                    if film_name in result[year]:
+                        if not film_location in result[year][film_name]:
+                            result[year][film_name].append(film_location)
+                    else:
+                        result[year][film_name] = [film_location]
+                else:
+                    result[year] = {film_name: [film_location]}
+
+# map
+m = folium.Map(location=[30, 31], zoom_start=2)
+
+for film_name in result[year_input]:
+    for film_locations in result[year_input][film_name]:
+        folium.Marker(film_locations, popup = film_name).add_to(m)
+
+#folium.LayerControl().add_to(m)
+
+m.save('film_map.html')
+print("File film_map.html generated.")
